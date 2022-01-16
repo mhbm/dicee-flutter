626fc85f30fae80ced5ddcaad846bf4ea60fa7fa
Mateus Macedo <mhbassomacedo@gmail.com>
2022-01-16 18:47:43 -0300

    Create random images after click
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,18 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 3;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset("images/dice1.png"),
-        ),
-        Expanded(
-          child: Image.asset("images/dice2.png"),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber =  Random().nextInt(6) + 1;
+                });
+                print('Left Button got pressed.');
+                
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber =  Random().nextInt(6) + 1;
+                });
+                print('Right button got pressed.');
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
